5371704eb6a6ac1c8a35329fa5bd3842c5c91f14
Thales Minoru Sato <thalesms2@outlook.com>
2022-11-09 17:18:42 -0300

    feat: initial prisma integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { NextPage } from 'next'
 import styles from "styles/Home.module.css";
+import prisma from 'lib/prisma';
 
 const Home: NextPage = () => {
     return (
